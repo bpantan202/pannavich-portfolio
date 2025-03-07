adee099dfc050ffd4ae8b85eb101c327d52cd3ba
Pannavich Tanyavichitkul <pannavich.tan@gmail.com>
2025-03-07 23:07:15 +0700

    Home page & Project

--- src/components/TopMenuItem.tsx
+++ src/components/TopMenuItem.tsx
@@ -1,6 +1,7 @@
 import { Link } from "react-router-dom";
 
-export default function TopMeunItem ({title, pageRef} : {title:String, pageRef:String}) {
+export default function TopMeunItem ({title, pageRef} : {title:String, pageRef:string}) {
+
     return (
         <Link to={pageRef} style={{margin:"20px", color:"#4D869C", fontFamily:"Copperplate, Papyrus, fantasy", fontSize:"20px"}}>
             {title}
